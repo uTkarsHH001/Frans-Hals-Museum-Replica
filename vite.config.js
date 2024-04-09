6c13b8ee53d1e55fb35f47d854356ba20402c92f
uTkarsHH001 <utk.arshh.9.2@gmail.com>
2024-04-09 20:31:51 +0530

    Modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Specify additional assets to be copied to the output directory (dist)
-    assetsInclude: ['./dist/assets/index-bthTjQ_z.js', './dist/assets/index-Bw5XaEfj.css'],
-  },
+  base: "/Frans-Hals-Museum-Replica",
+  plugins: [react()]
 })
