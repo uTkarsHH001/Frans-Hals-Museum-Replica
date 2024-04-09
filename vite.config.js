205c7a05c6b9e94476eed8b1411b69d306b05df5
uTkarsHH001 <utk.arshh.9.2@gmail.com>
2024-04-09 20:14:18 +0530

    Modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify additional assets to be copied to the output directory (dist)
+    assetsInclude: ['./dist/assets/index-bthTjQ_z.js', './dist/assets/index-Bw5XaEfj.css'],
+  },
 })
